0dac2efc62d5c8d794d7e1cb1aca19c0f5194f43
jdey4 <jdey4@jhmi.edu>
2022-09-06 00:16:08 -0400

    new res

--- try.py
+++ try.py
@@ -18,7 +18,7 @@ base_task = []
 task = []
 accuracy = []
 
-filename = '/Users/jayantadey/progressive-learning-pytorch/store/results/dict-dataset5-N20--C3-5x16-bn_F-9216x2000x2000_c100--i100-lr0.0001-b256--SI10000.0-0.1.pkl'
+filename = '/Users/jayantadey/progressive-learning-pytorch/store/results/dict-dataset5-N20--C3-5x16-bn_F-9216x2000x2000_c100--i100-lr0.0001-b256.pkl'
 
 with open(filename, 'rb') as f:
     data = pickle.load(f)['R']
@@ -34,9 +34,9 @@ multitask_df['base_task'] = base_task
 multitask_df['accuracy'] = accuracy
 
 df_single_task['task'] = range(1, 21)
-df_single_task['accuracy'] = list(data.iloc[6])
+df_single_task['accuracy'] = list(data.iloc[21])
 
 summary = (multitask_df,df_single_task)
-with open('./reformed_res/imagenet/si.pickle', 'wb') as f:
+with open('./reformed_res/imagenet/None.pickle', 'wb') as f:
     pickle.dump(summary, f)
 # %%
